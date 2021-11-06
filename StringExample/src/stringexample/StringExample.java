fe63610451eab1353cf88578e935616da50d0aa7
Yehor06 <93422017+Yehor06@users.noreply.github.com>
2021-11-06 10:12:49 +0200

    add examples
    add examples

--- StringExample/src/stringexample/StringExample.java
+++ StringExample/src/stringexample/StringExample.java
@@ -12,10 +12,42 @@ public class StringExample {
          System.out.println("Как вас зовут?");
          Scanner input = new Scanner(System.in);
          
-         //String name = input.nextLine();
-         String name = input.next();
-         
+         String name = input.nextLine();
+         //String name = input.next();
          System.out.println("Ну, привет," + name);
+         
+         int n = name.length();
+        int l = 1;
+         int numLastChar = n - l;
+         
+         for(int i = numLastChar; i >= 0; i--)
+         {
+             System.out.println(name.charAt(i));
+             
+         }
+         
+         if(n>=4)
+         {
+             String newString = name.substring(l,3 + l);
+             System.out.println(newString);
+         }
+         String[] words = name.split(" ");
+         int M = words.length;
+         String firstName = words[0];
+         String lastName = words[l];
+         
+         
+         System.out.printf("Вы ввели слов %d, это имя = %s и фамилия = %s \n", M, firstName, lastName);
+         
+         
+         boolean result = name.contains(lastName);
+         if (result == true)
+         {
+             System.out.println("Строка ="+name+" содержит строку = "+lastName);
+             
+             
+         }
+         
     }
     
 }
